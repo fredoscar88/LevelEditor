3728ee6e549198eef23727ae482179332f1a3de2
Henry Farr <hfarr80@yahoo.com>
2016-12-27 22:39:06 -0500

    Implemented Terrain to do serial/deserial
    This is a second approach to deserialization I have. Basically, it's the
    more polymorphic/OOP approach. Let Terrain, the superclass of Floor and
    Wall, do serializing/deserializing. Floor and Wall are indistinguishable
    from each other except for their methods and sprites- neither of which
    is considered in serialization. The fields that are serialized by all
    Terrain subclasses should, logically, be serialized by Terrain!
    
    Important note- Subclasses of Terrain definitely need to implement
    deserialize (because we otherwise can't tell what type of terrain we
    deserialized). Subclasses that are only distinguished by sprites and
    methods need not implement serialize, since all of that work is done by
    Terrain, but they DO need to implement it if they have specific member
    variables that Terrain doesn't have. They still should make a super call
    to Terrain's serialize to get all of Terrain's member variables out of
    the way.

--- src/com/visellico/platty/leveleditor/Level/LevelObjects/Terrain/Floor.java
+++ src/com/visellico/platty/leveleditor/Level/LevelObjects/Terrain/Floor.java
@@ -36,38 +36,48 @@ public class Floor extends Terrain {
 		
 	}
 	
-	/**
-	 * Serialize a floor for saving into a file
-	 * @return instance of this floor saved as an RCObject
-	 * @devnote <p>It may be more beneficial to serialize at the direct 
-	 * superclass of floor (Terrain) because all of the serialized
-	 * information is at the Terrain level anyway</p>
-	 */
-	public RCObject serialize() {
-		
-		RCObject objFloor = new RCObject("floor");
-		
-		objFloor.addField(RCField.Int("x", x));
-		objFloor.addField(RCField.Int("y", y));
-		objFloor.addField(RCField.Int("width", width));
-		objFloor.addField(RCField.Int("height", height));
-		
-		return objFloor;
-		
-	}
+	//@devnote floor does not need to implement Serialize, since it is an uncomplicated Terrain instance.
+//	/**
+//	 * Serialize a floor for saving into a file
+//	 * @return instance of this floor saved as an RCObject
+//	 * @devnote <p>It may be more beneficial to serialize at the direct 
+//	 * superclass of floor (Terrain) because all of the serialized
+//	 * information is at the Terrain level anyway</p>
+//	 */
+//	public RCObject serialize() {
+//		
+//		RCObject objFloor = new RCObject("floor");
+//		
+//		objFloor.addField(RCField.Int("x", x));
+//		objFloor.addField(RCField.Int("y", y));
+//		objFloor.addField(RCField.Int("width", width));
+//		objFloor.addField(RCField.Int("height", height));
+//		
+//		return objFloor;
+//		
+//	}
 	
-	public static Floor deserialize(RCObject objFloor) {
+	/**
+	 * Deserializes a floor
+	 * @param objFloor
+	 * @return
+	 * @devnote <p>How could we move this deserialize method, or atleast most of it, to Terrain?
+	 * My thinking is that. like with serializing, all of the important configuration data is
+	 * specific to Terrain and not all of its subclasses. Perhaps have the deserialize </p>
+	 */
+	public Floor deserialize(RCObject objFloor) {
 		
-		int x, y, width, height;
-		x = objFloor.findField("x").getInt();
-		y = objFloor.findField("y").getInt();
-		width = objFloor.findField("width").getInt();
-		height = objFloor.findField("height").getInt();
+//		int x, y, width, height;
+//		x = objFloor.findField("x").getInt();
+//		y = objFloor.findField("y").getInt();
+//		width = objFloor.findField("width").getInt();
+//		height = objFloor.findField("height").getInt();
+//		
+//		Floor f = new Floor(x, y);
+//		f.width = width;
+//		f.height = height;	//MUST be same as y
 		
-		Floor f = new Floor(x, y);
-		f.width = width;
-		f.height = height;	//MUST be same as y
-		return f;
+		return (Floor) super.deserialize(objFloor);
 	}
 	
 	public void render(Screen screen) {
--- src/com/visellico/platty/leveleditor/Level/LevelObjects/Terrain/Terrain.java
+++ src/com/visellico/platty/leveleditor/Level/LevelObjects/Terrain/Terrain.java
@@ -1,9 +1,58 @@
 package com.visellico.platty.leveleditor.Level.LevelObjects.Terrain;
 
+import com.visellico.graphics.Screen;
+import com.visellico.platty.level.Level;
 import com.visellico.platty.leveleditor.Level.LevelObjects.LevelObject;
+import com.visellico.rainecloud.serialization.RCField;
+import com.visellico.rainecloud.serialization.RCObject;
+import com.visellico.rainecloud.serialization.RCString;
 
-public abstract class Terrain extends LevelObject {
+public class Terrain extends LevelObject {
 
 	int width, height;
+	protected String serialName;
+	
+	/**
+	 * serializes all aspects of Terrain that are non-specific
+	 * @param t
+	 * @return
+	 */
+	public RCObject serialize(Terrain t) {
+		
+		RCObject objTerrain = new RCObject("terrain");
+		
+		objTerrain.addString(RCString.Create("type", t.serialName));
+		
+		objTerrain.addField(RCField.Int("x", t.x));
+		objTerrain.addField(RCField.Int("y", t.y));
+		objTerrain.addField(RCField.Int("width", t.width));
+		objTerrain.addField(RCField.Int("height", t.height));
+		
+		return objTerrain;
+		
+	}
+	
+	protected Terrain deserialize(RCObject objTerrain) {
+	
+		Terrain t = new Terrain();
+		
+		x = objTerrain.findField("x").getInt();
+		y = objTerrain.findField("y").getInt();
+		width = objTerrain.findField("width").getInt();
+		height = objTerrain.findField("height").getInt();
+		
+		return t;
+	}
 
+	public void init(Level l) {
+		
+	}
+
+	public void render(Screen screen) {
+		
+	}
+
+	
+
+	
 }
